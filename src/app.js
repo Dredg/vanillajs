3a04a781f494595b0fb5d7004b04454c1e46ebeb
Walking on the moon <45702832+Dredg@users.noreply.github.com>
2023-07-08 15:48:10 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,28 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function generateExcuse() {
+  let who = ["El perro", "Mi abuela", "Su tortuga", "Mi pájaro"];
+  let action = ["se comió", "orinó en", "aplastó", "rompió"];
+  let what = ["mi tarea", "las llaves", "el coche"];
+  let when = [
+    "antes de la clase",
+    "justo a tiempo",
+    "cuando terminé",
+    "durante mi almuerzo",
+    "mientras estaba rezando"
+  ];
+
+  let excuse =
+    who[Math.floor(Math.random() * who.length)] +
+    " " +
+    action[Math.floor(Math.random() * action.length)] +
+    " " +
+    what[Math.floor(Math.random() * what.length)] +
+    " " +
+    when[Math.floor(Math.random() * when.length)];
+
+  document.getElementById("excuse").innerHTML = excuse;
+}
+
+window.onload = generateExcuse;
