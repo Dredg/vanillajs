e4ffa8e9d9e40272480bad08d9d10865b8cb3021
Walking on the moon <45702832+Dredg@users.noreply.github.com>
2023-07-14 17:53:26 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -5,28 +5,14 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-function generateExcuse() {
-  let who = ["El perro", "Mi abuela", "Su tortuga", "Mi pájaro"];
-  let action = ["se comió", "orinó en", "aplastó", "rompió"];
-  let what = ["mi tarea", "las llaves", "el coche"];
-  let when = [
-    "antes de la clase",
-    "justo a tiempo",
-    "cuando terminé",
-    "durante mi almuerzo",
-    "mientras estaba rezando"
-  ];
+var pronoun = ["the", "our"];
+var adj = ["great", "big"];
+var noun = ["jogger", "racoon"];
 
-  let excuse =
-    who[Math.floor(Math.random() * who.length)] +
-    " " +
-    action[Math.floor(Math.random() * action.length)] +
-    " " +
-    what[Math.floor(Math.random() * what.length)] +
-    " " +
-    when[Math.floor(Math.random() * when.length)];
-
-  document.getElementById("excuse").innerHTML = excuse;
+for (var i = 0; i < pronoun.length; i++) {
+  for (var j = 0; j < adj.length; j++) {
+    for (var k = 0; k < noun.length; k++) {
+      console.log(pronoun[i] + adj[j] + noun[k] + ".com");
+    }
+  }
 }
-
-window.onload = generateExcuse;
